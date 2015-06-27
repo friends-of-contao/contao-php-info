92c83bb573477ebc59158592399fb7bee202957a
CeeKay <no-one@users.noreply.github.com>
2015-06-27 15:32:06 +0200

    Update the copyright notices.

--- system/modules/PHPInfo/languages/en/default.php
+++ system/modules/PHPInfo/languages/en/default.php
@@ -2,30 +2,17 @@
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2014 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
+ * Copyright (c) 2005-2015 Leo Feyer
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
  * @copyright  Cliff Parnitzky 2014
  * @author     Cliff Parnitzky
+ * @author     Christoph Krebs
  * @package    PHPInfo
- * @license    LGPL
+ * @license    LGPL-3.0+
+ * @filesource [phpinfo] by Christian Steurer (Russe)
  */
+
 
 $GLOBALS['TL_LANG']['PHPInfo']['disabled']['headline']         = 'Function unavailable';
 $GLOBALS['TL_LANG']['PHPInfo']['disabled']['disableFunctions'] = 'The function <code>phpinfo()</code> was disabled by <code>disable_functions</code>. Please contact your server administrator.';
